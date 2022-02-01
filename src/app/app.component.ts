06b60c678f31723fa12248bbda7326ead414a7c2
Gabriel Nemeth <github.zaphrentid@aleeas.com>
2022-02-01 20:27:32 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'web';
+    public constructor(private http: HttpClient) {}
+
+    public onSubmit(email: string, password: string, evt: Event): void {
+        evt.preventDefault();
+        console.log(email, password);
+        this.http
+            .post('/api/auth/login', {email, password})
+            .subscribe(console.log);
+    }
+
+    public onSignOut(): void {
+        this.http.get('/api/auth/logout').subscribe(console.log);
+    }
 }
