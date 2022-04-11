aebae278a2ae434f78020b1d29781a7db5f3803d
Gabriel Nemeth <github.zaphrentid@aleeas.com>
2022-04-11 20:48:50 +0200

    Add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
@@ -16,6 +21,13 @@ import {HomeModule} from './modules/home/home.module';
         BrowserAnimationsModule,
         CoreModule,
         HomeModule,
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: environment.production,
+        }),
+        EffectsModule.forRoot([]),
+        StoreRouterConnectingModule.forRoot(),
     ],
     bootstrap: [AppComponent],
 })
