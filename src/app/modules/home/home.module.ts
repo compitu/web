c896a68ee693d3066dd320fa3b1a1f001ac0e873
Gabriel Nemeth <github.zaphrentid@aleeas.com>
2022-04-19 11:29:28 +0200

    Add sidenav

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,19 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {RouterModule} from '@angular/router';
 import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './pages/home/home.component';
 
 @NgModule({
-    imports: [CommonModule, MatButtonModule, RouterModule, HomeRoutingModule],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatSidenavModule,
+        RouterModule,
+        HomeRoutingModule,
+    ],
     declarations: [HomeComponent],
     exports: [HomeComponent],
 })
