3df19904cfaa0c87db2738ac7a9e3abe61fea90f
Gabriel Nemeth <github.zaphrentid@aleeas.com>
2022-04-19 11:17:33 +0200

    Reorganize the home folder structure

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {RouterModule} from '@angular/router';
 import {HomeRoutingModule} from './home-routing.module';
-import {HomeComponent} from './home.component';
+import {HomeComponent} from './pages/home/home.component';
 
 @NgModule({
     imports: [CommonModule, MatButtonModule, RouterModule, HomeRoutingModule],
